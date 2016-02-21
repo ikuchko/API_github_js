7857a9897823e3138c67687febbd274065a54508
Illia <ikuchko@gmail.com>
2016-02-20 22:06:57 -0800

    fix jshint errors

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,15 @@ exports.formatDate = function (dateCreated) {
   var months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "july", "Aug", "Sept", "Oct", "Nov", "Dec"];
   var date = new Date(dateCreated);
   return ("Joined on " + months[date.getMonth()] + " " + date.getDate() + ", " + date.getYear());
-}
+};
 
 exports.formatUpdated = function (dateUpdated) {
   return moment(dateUpdated).fromNow();
-}
+};
 
 exports.sortByName = function (array) {
   return array.sort(function(a, b){
     var dateA=new Date(a.updated_at), dateB=new Date(b.updated_at);
     return dateB-dateA; //sort by date decending
   });
-}
+};
